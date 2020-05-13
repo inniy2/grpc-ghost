738c3bebb83eb83ef9be754184852f0dc1ee7bc5
inniy2 <inniy2@gmail.com>
2020-05-13 15:27:00 +0300

    support /etc/grpc-ghost

--- deploy-desktop.sh
+++ deploy-desktop.sh
@@ -6,11 +6,21 @@
 # scp -i /Users/baesangsun/.vagrant.d/boxes/percona/0/virtualbox/vagrant_private_key ./ghost_pb2_grpc.py vagrant@192.168.33.12:/home/vagrant/grpc-ghost
 
 cp ./server.py           /var/lib/lxd/containers/u2/rootfs/root/grpc-ghost/;
+cp ./config.ini          /var/lib/lxd/containers/u2/rootfs/etc/grpc-ghost/;
 cp ./ghost_pb2.py        /var/lib/lxd/containers/u2/rootfs/root/grpc-ghost/;
 cp ./ghost_pb2_grpc.py   /var/lib/lxd/containers/u2/rootfs/root/grpc-ghost/;
 
 
+cp ./server.py           /var/lib/lxd/containers/u1/rootfs/root/grpc-ghost/;
+cp ./config.ini          /var/lib/lxd/containers/u1/rootfs/etc/grpc-ghost/;
+cp ./ghost_pb2.py        /var/lib/lxd/containers/u1/rootfs/root/grpc-ghost/;
+cp ./ghost_pb2_grpc.py   /var/lib/lxd/containers/u1/rootfs/root/grpc-ghost/;
+
+# sudo chown  -R '165536'.'165536'  /var/lib/lxd/containers/u1/rootfs/root/grpc-ghost
 # sudo chown  -R '165536'.'165536'  /var/lib/lxd/containers/u2/rootfs/root/grpc-ghost
+
+# sudo chown  -R '165536'.'165536'  /var/lib/lxd/containers/u1/rootfs/etc
+# sudo chown  -R '165536'.'165536'  /var/lib/lxd/containers/u2/rootfs/etc
 
 # apt install python3-pip
 # pip3 install grpcio
